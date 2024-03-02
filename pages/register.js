67b6f0e4cee56fc3ecdf860617f43bda39f11f38
xueyuan Xu <xueyuan183@gmail.com>
2024-03-02 00:53:56 -0500

    new commit

--- pages/register.js
+++ pages/register.js
@@ -1,4 +1,4 @@
-import Navbar from "@/components/NavbarSolid";
+import Navbar from "@/components/Navbar";
 import Register from "@/components/registerContent";
 import Footer from "@/components/Footer";
 
